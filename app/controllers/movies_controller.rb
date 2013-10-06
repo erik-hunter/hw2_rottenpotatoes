a380939eda2cc722e147335d060189267af3ffb0
Erik Hunter <erik.hunter@colorado.edu>
2013-10-06 16:50:33 -0600

    got part b working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:OrderBy])
+    @hilitedColumn = params[:OrderBy]
   end
 
   def new
