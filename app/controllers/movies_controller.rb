e73b412643d4de3b7e130b1883f1458c277f8fd4
Erik Hunter <erik.hunter@colorado.edu>
2013-10-09 17:11:08 -0600

    all done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,23 @@
 class MoviesController < ApplicationController
 
   def show
+  	if(params[:ratings] == nil)
+  		@selected_ratings = @all_ratings
+  	else
+  		@selected_ratings = params[:ratings]
+  	end
+  	
+  	if(params[:OrderBy] == nil)
+  		@OrderBy = ''
+  	else
+  		@OrderBy = params[:OrderBy]
+  	end
+  	
+  	
+  	# save off the session
+  	session[:ratings] = @selected_ratings
+  	session[:OrderBy] = @OrderBy
+  	
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
@@ -9,19 +26,24 @@ class MoviesController < ApplicationController
   def index
   	@all_ratings = Movie.getUniqueMovieRatings
   	
-  	
-  	if(@selected_ratings == nil)
-  		@selected_ratings = @all_ratings
+  	if(!params[:ratings] and session[:ratings])
+  		temp = {:ratings => session[:ratings], :OrderBy => session[:OrderBy]}
+  		redirect_to movies_path(temp)
   	end
   	
-  	#if(params[:ratings])
-    	@movies = Movie.where(rating: params[:ratings].keys).order(params[:OrderBy])
-    	@selected_ratings = params[:ratings].keys
-    #else
-    	#@movies = Movie.where(rating: @selected_ratings).order(params[:OrderBy])
-    	#@selected_ratings = @all_ratings
-    #end
-    @hilitedColumn = params[:OrderBy]
+  	if(params[:ratings] == nil)
+  		@selected_ratings = @all_ratings
+  	else
+  		@selected_ratings = params[:ratings].keys
+  	end
+  	
+  	if(params[:OrderBy] == nil)
+  		@OrderBy = ''
+  	else
+  		@OrderBy = params[:OrderBy]
+  	end
+  	
+    @movies = Movie.where(rating: @selected_ratings).order(@OrderBy)
   end
 
   def new
